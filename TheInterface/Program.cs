210d7acef338c6788e794e816f4d204b2aa7399f
lemo <docuemperor@proton.me>
2026-05-05 08:41:42 +0200

    First prototype

---
+++ TheInterface/Program.cs
@@ -0,0 +1,32 @@
+using MudBlazor.Services;
+using TheInterface.Components;
+using TheInterface.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddMudServices();
+builder.Services.AddSingleton<MicroBitReader>();
+builder.Services.AddHostedService(sp => sp.GetRequiredService<MicroBitReader>());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
